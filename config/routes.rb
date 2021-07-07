ba660b514d4c1b58a47bd2172f8223161a3553af
shubham <shubham@shubhams-MacBook-Air.local>
2021-07-07 07:53:20 +0530

    extended_mylists_route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :my_lists
+  # this is used to extend routes after /.i mean the upper do will stretch the 
+  # localhost:3000 to /my_lists and the lower one will further stretch it to 
+  # my_lists/:id/todo_items/:id
+  resources :my_lists do
+    resources:todo_items
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'my_lists#index'
 end
